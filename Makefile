a19a634b5470955c000ea5f82d3ec2d91448d9ca
Alejandro Galan Perez <agp221@alu.ua.es>
2023-10-05 16:23:54 +0200

    Final v1.0.0

--- Makefile
+++ Makefile
@@ -1,16 +1,23 @@
 #DESTDIR=/usr/local
 DESTDIR=./usr
+COMPLETIONDIRBASH=$(DESTDIR)/share/bash-completion/completions
+COMPLETIONDIRZSH=$(DESTDIR)/share/zsh/site-functions
+COMPLETIONSFILE=_dcalc
 PROG=dcalc
 
 all: $(PROG)
 
 $(PROG): $(PROG).cpp exprtk.hpp
-	g++ -O3 -o $@ $(PROG).cpp
+	g++ -O3 -o $@ $(PROG).cpp -w
 
 install: $(PROG)
 	#echo DESTDIR=$(DESTDIR)
 	mkdir -p $(DESTDIR)/bin
 	cp $(PROG) $(DESTDIR)/bin/
+	mkdir -p $(COMPLETIONDIRBASH)
+	cp ./completions/$(COMPLETIONSFILE) $(COMPLETIONDIRBASH)/$(COMPLETIONFILE)
+	mkdir -p $(COMPLETIONDIRZSH)
+	cp ./completions/$(COMPLETIONSFILE) $(COMPLETIONDIRZSH)/$(COMPLETIONFILE)
 
 clean:
 	rm -f *~ $(PROG) *.o
--- dcalc.cpp
+++ dcalc.cpp
@@ -4,88 +4,93 @@
 #include "exprtk.hpp"
 #include <iostream>
 
-enum Option {UI, CALCULATE_NOARG, CALCULATE, HELP, VERSION, ERR_ARG};
-// Función para mostrar la ayuda
+enum Option { UI, CALCULATE_NOARG, CALCULATE, HELP, VERSION, ERR_ARG, RAW_CALCULATE };
+
+// Function to display help
 void showHelp() {
-    printf("Uso: dcalc [OPCIÓN] [EXPRESIÓN]\n");
-    printf("Opciones:\n");
-    printf("  -c, --calculate <EXPRESIÓN>  Calcula el resultado de la expresión\n");
-    printf("  --help                       Muestra este mensaje de ayuda\n");
-    printf("  --version                    Muestra la versión del programa\n");
-}
-void showVersion(){
-    printf("DCAlc 1.0.0\n");
-    printf("Autor: Alejandro Galan Perez\n");
+    printf("Usage: dcalc [OPTION] [EXPRESSION]\n");
+    printf("Options:\n");
+    printf("  -c, --calculate <EXPRESSION>  Calculate the result of the expression\n");
+    printf("  -rc, --raw-calculate <EXPRESSION>  Calculate the result of the expression showing it without added text as 'Result: '\n");
+    printf("  --help                       Display this help message\n");
+    printf("  --version                    Display program version\n");
 }
 
-int parseArg(int argc, char *argv[]){
+void showVersion() {
+    printf("DCAlc 1.0.0\n");
+    printf("Author: Alejandro Galan Perez\n");
+}
+
+int parseArg(int argc, char *argv[]) {
     if (argc == 1) return UI;
     if (argc == 2) {
-        if(strcmp(argv[1], "--help") == 0 || strcmp(argv[1], "-h") == 0) return HELP;
-        if(strcmp(argv[1], "--version") == 0 || strcmp(argv[1], "-v") == 0) return VERSION;
-        if(strcmp(argv[1], "--calculate") == 0 || strcmp(argv[1], "-c") == 0) return CALCULATE_NOARG;
+        if (strcmp(argv[1], "--help") == 0 || strcmp(argv[1], "-h") == 0) return HELP;
+        if (strcmp(argv[1], "--version") == 0 || strcmp(argv[1], "-v") == 0) return VERSION;
+        if (strcmp(argv[1], "--raw-calculate") == 0 || strcmp(argv[1], "-rc") == 0) return CALCULATE_NOARG;
         return ERR_ARG;
     }
     if (argc >= 3) {
-        if(strcmp(argv[1], "--calculate") == 0 || strcmp(argv[1], "-c") == 0) return CALCULATE;
+        if (strcmp(argv[1], "--calculate") == 0 || strcmp(argv[1], "-c") == 0) return CALCULATE;
+        if (strcmp(argv[1], "--raw-calculate") == 0 || strcmp(argv[1], "-rc") == 0) return RAW_CALCULATE;
     }
     return ERR_ARG;
-
 }
 
-
-char *getExpression(int argc, char *argv[], int pos){
-    int longitudTotal = 0;
+char *getExpression(int argc, char *argv[], int pos) {
+    int totalLength = 0;
     for (int i = pos; i < argc; i++) {
-        longitudTotal += strlen(argv[i]) + 1; // +1 para el espacio entre argumentos
+        totalLength += strlen(argv[i]) + 1; // +1 for the space between arguments
     }
 
-    char* resultado = (char*)malloc(longitudTotal * sizeof(char));
-    if (resultado == NULL) {
-        perror("Error al asignar memoria");
+    char *result = (char *)malloc(totalLength * sizeof(char));
+    if (result == NULL) {
+        perror("Error allocating memory");
         exit(1);
     }
 
-    strcpy(resultado, ""); // Inicializa el resultado como una cadena vacía
+    strcpy(result, ""); // Initialize the result as an empty string
 
     for (int i = pos; i < argc; i++) {
-        strcat(resultado, argv[i]);
+        strcat(result, argv[i]);
         if (i < argc - 1) {
-            strcat(resultado, " "); // Agrega un espacio entre argumentos
+            strcat(result, " "); // Add a space between arguments
         }
     }
-    strcat(resultado, "\0"); // Agrega el carácter nulo al final de la cadena
+    strcat(result, "\0"); // Add the null character at the end of the string
 
-    return resultado;
+    return result;
 }
 
-// Función para evaluar la expresión en RPN
-    void trig_function(const char *exp)
-    {
-        typedef exprtk::symbol_table<double> symbol_table_t;
-        typedef exprtk::expression<double> expression_t;
-        typedef exprtk::parser<double> parser_t;
+// Function to evaluate the RPN expression
+void trig_function(const char *exp, bool raw) {
+    typedef exprtk::symbol_table<double> symbol_table_t;
+    typedef exprtk::expression<double> expression_t;
+    typedef exprtk::parser<double> parser_t;
 
-        std::string expression_string(exp);
+    std::string expression_string(exp);
+    int count = std::count_if(expression_string.begin(), expression_string.end(),
+                              [](unsigned char c){ return std::isspace(c); });
+    remove(expression_string.begin(), expression_string.end(), ' ');
+    expression_string = expression_string.substr(0, expression_string.length()-count);
 
-        symbol_table_t symbol_table;
-        expression_t expression;
-        parser_t parser;
 
-        if (parser.compile(expression_string, expression)) {
-            double result = expression.value();
-            std::cout << "Resultado de la expresión: " << result << std::endl;
-        } else {
-            std::cerr << "Error al compilar la expresión." << std::endl;
-        }
+    symbol_table_t symbol_table;
+    expression_t expression;
+    parser_t parser;
+
+    if (parser.compile(expression_string, expression)) {
+        double result = expression.value();
+        std::cout << ((raw) ? "" : "Result of the expression: ") << result << std::endl;
+    } else {
+        std::cerr << "Error compiling the expression." << std::endl;
     }
+}
 
-
-void userInterface(){
-    char expresion[300];
-    printf("Introduce la expresión a calcular: ");
-    scanf("%s", expresion);
-    trig_function(expresion);
+void userInterface() {
+    printf("Enter the expression to calculate: ");
+    std::string expression;
+    std::getline(std::cin, expression);
+    trig_function(expression.c_str(), false);
 }
 
 int main(int argc, char *argv[]) {
@@ -96,11 +101,11 @@ int main(int argc, char *argv[]) {
             userInterface();
             break;
         case 1:
-            printf("Error: Falta la expresión a calcular\n");
+            printf("Error: Missing expression to calculate\n");
             break;
-        case 2:{
-            const char *expresion = getExpression(argc, argv, 2);
-            trig_function(expresion);
+        case 2: {
+            const char *expression = getExpression(argc, argv, 2);
+            trig_function(expression, false);
         }
             break;
         case 3:
@@ -110,11 +115,12 @@ int main(int argc, char *argv[]) {
             showVersion();
             break;
         case 5:
-            printf("Error: Argumento no reconocido\nUsa -h | --help para más información\n");
+            printf("Error: Unrecognized argument\nUse -h | --help for more information\n");
             break;
+        case 6: {
+            const char *expression = getExpression(argc, argv, 2);
+            trig_function(expression, true);
+        }
     }
     return 0;
 }
-
-
-
